711c6869f62fe6252e108bc42fa38bf3324fd191
ImHyeonu <kmjnh34@gmail.com>
2025-04-01 13:02:06 +0900

    last

--- info-b/src/App.js
+++ info-b/src/App.js
@@ -5,16 +5,10 @@ import Layout from "./components/Layout";
 import Company from "./pages/Company";
 import Business from "./pages/Business";
 import Research from "./pages/Research";
-import Recruitment from "./pages/recruiment/Recruitment";
+import Recruitment from "./pages/Recruitment";
 import CeoMessage from "./pages/company/CeoMessage";
 import History from "./pages/company/History";
 import Certification from "./pages/company/Certification";
-import BusinessInfo from "./pages/business/BusinessInfo";
-import RnBusiness from "./pages/business/RnBusiness";
-import RecruimentTalent from "./pages/recruiment/RecruimentTalent";
-import RecruimentBenefits from "./pages/recruiment/RecruimentBenefits";
-import Performance from "./pages/research/Performance";
-import PerformanceCase from "./pages/research/PerformanceCase";
 
 function App() {
   return (
@@ -28,16 +22,8 @@ function App() {
             <Route path="company/history" element={<History />} />
             <Route path="company/certification" element={<Certification />} />
             <Route path="business" element={<Business />} />
-            <Route path="business/info" element={<BusinessInfo />} />
-            <Route path="business/research" element={<RnBusiness />} />
-            <Route path="performance" element={<Performance />} />
-            <Route path="performance/cases" element={<PerformanceCase />} />
+            <Route path="research" element={<Research />} />
             <Route path="recruitment" element={<Recruitment />} />
-            <Route path="recruitment/talent" element={<RecruimentTalent />} />
-            <Route
-              path="recruitment/benefits"
-              element={<RecruimentBenefits />}
-            />
           </Route>
         </Routes>
       </BrowserRouter>
